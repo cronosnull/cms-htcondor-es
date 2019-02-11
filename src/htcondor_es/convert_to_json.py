44a2a2c1d365a1137b709c7888470810a512bc05
Christian Ariza <christian.ariza@gmail.com>
2019-02-11 17:22:25 +0100

    postJobURL

--- src/htcondor_es/convert_to_json.py
+++ src/htcondor_es/convert_to_json.py
@@ -611,6 +611,7 @@ def convert_to_json(ad, cms=True, return_dict=False, reduce_data=False):
     result["CoreHr"] = ad.get("RequestCpus", 1.0)*int(ad.get("RemoteWallClockTime", 0))/3600.
     result["CommittedCoreHr"] = ad.get("RequestCpus", 1.0)*ad.get("CommittedTime", 0)/3600.
     result["CommittedWallClockHr"] = ad.get("CommittedTime", 0)/3600.
+    result["CommittedWallClockSc"] = ad.get("CommittedTime", 0)
     result["CpuTimeHr"] = (ad.get("RemoteSysCpu", 0)+ad.get("RemoteUserCpu", 0))/3600.
     result["DiskUsageGB"] = ad.get("DiskUsage_RAW", 0)/1000000.
     result["MemoryMB"] = ad.get("ResidentSetSize_RAW", 0)/1024.
@@ -744,7 +745,23 @@ def convert_to_json(ad, cms=True, return_dict=False, reduce_data=False):
         result['CPUModel'] = str(ad['MachineAttrCPUModel0'])
         result['CPUModelName'] = str(ad['MachineAttrCPUModel0'])
         result['Processor'] = str(ad['MachineAttrCPUModel0'])
-
+    # Create URL's
+    result['jobLogURL'] = 'https://cmsweb.cern.ch/scheddmon/{sched}/{cmsUserId}/{task}/job_out.{job}.{retry}.txt'\
+    .format({
+        'sched': result.get('ScheddName','UNKNOWN'),
+        'cmsUserId': result['User'],
+        'task': result.get('CRAB_Workflow', result['Workflow']),
+        'job': result.get('CRAB_Id','0'),
+        'retry': result.get('CRAB_Retry','0')
+        })
+    result['postJobLogURL'] = 'https://cmsweb.cern.ch/scheddmon/{sched}/{cmsUserId}/{task}/postjob.{job}.{retry}.txt'\
+    .format({
+        'sched': result.get('ScheddName','UNKNOWN'),
+        'cmsUserId': result['User'],
+        'task': result.get('CRAB_Workflow', result['Workflow']),
+        'job': result.get('CRAB_Id','0'),
+        'retry': result.get('CRAB_Retry','0')
+        })
     if reduce_data:
         result = drop_fields_for_running_jobs(result)
 
